99017f29ecd834a685e54ed86ca37eecfdc7641d
Almanik B <almanik.balingga3@gmail.com>
2020-04-22 20:43:23 +0800

    updatedb

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # PUNTENDOC
 
 Welcome gaid, kerjain bagian masing2 yak supaya tida conflict, defaultnya sudah ada kerjaan intan disini wkwkwk.
+clone di folder htdocs langsung ya pake puntendoc langsung, DB nya juga disesuaiin sama yang sudah disetting.
+DB bisa di impor aja di file .sql yg ada
+
 sementara buat MVC masing2 aja dulu, ntar jumat di satuin, sabtu siap terbang
---
+++ puntendoc.sql
@@ -0,0 +1,169 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 22, 2020 at 02:20 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `puntendoc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_artikel`
+--
+
+CREATE TABLE `tbl_artikel` (
+  `id` int(11) NOT NULL,
+  `judul` varchar(50) NOT NULL,
+  `penulis` varchar(50) NOT NULL,
+  `isi` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_obat`
+--
+
+CREATE TABLE `tbl_obat` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `deskripsi` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_penyakit`
+--
+
+CREATE TABLE `tbl_penyakit` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `gejala` varchar(1000) NOT NULL,
+  `tindakan` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_pertanyaan`
+--
+
+CREATE TABLE `tbl_pertanyaan` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `pertanyaan` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_users`
+--
+
+CREATE TABLE `tbl_users` (
+  `id` int(11) NOT NULL,
+  `nama_user` varchar(100) NOT NULL,
+  `username` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `password` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `role` enum('admin','user') CHARACTER SET latin1 NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`id`, `nama_user`, `username`, `password`, `role`, `email`) VALUES
+(1, 'intan', 'admin1', 'e00cf25ad42683b3df678c61f42c6bda', 'admin', 'adminintan@gmail.com'),
+(2, 'Restu', 'user1', '24c9e15e52afc47c225b757e7bee1f9d', 'user', 'userrestu@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_artikel`
+--
+ALTER TABLE `tbl_artikel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_obat`
+--
+ALTER TABLE `tbl_obat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_penyakit`
+--
+ALTER TABLE `tbl_penyakit`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_pertanyaan`
+--
+ALTER TABLE `tbl_pertanyaan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_artikel`
+--
+ALTER TABLE `tbl_artikel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_obat`
+--
+ALTER TABLE `tbl_obat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_penyakit`
+--
+ALTER TABLE `tbl_penyakit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_pertanyaan`
+--
+ALTER TABLE `tbl_pertanyaan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
